9ffee3aeb1f6cf564b5f816bf8364f7ef95e9ec2
Migel <migellars@gmail.com>
2021-06-01 14:02:44 +0100

    features

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <Header/>
+    <Header />
+    <Featured />
+    <Banner />
+    <CTA />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
+import Banner from "./components/Banner.vue";
+import Featured from "./components/Featured.vue";
+import CTA from "./components/CTA.vue";
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    Header
-  }
-}
+    name: "App",
+    components: {
+        // HelloWorld,
+        Header,
+        Banner,
+        Featured,
+        CTA
+    }
+};
 </script>
 
-<style>
-</style>
+<style></style>
